59d1284d6d0eb241c98b350e618ccb380178cd7f
Damian Gryski <damian@gryski.com>
2020-07-13 09:14:54 -0700

    ruleguard: tighten unconvert rules to not trigger on constants

--- ruleguard.rules.go
+++ ruleguard.rules.go
@@ -6,23 +6,25 @@ import "github.com/quasilyte/go-ruleguard/dsl/fluent"
 
 // This is a collection of rules for ruleguard: https://github.com/quasilyte/go-ruleguard
 
+// Remove extra conversions: mdempsky/unconvert
+// False positive: var v interface{} = float32(1.0)
 func unconvert(m fluent.Matcher) {
-	m.Match("int($x)").Where(m["x"].Type.Is("int")).Report("unnecessary conversion").Suggest("$x")
+	m.Match("int($x)").Where(m["x"].Type.Is("int") && !m["x"].Const).Report("unnecessary conversion").Suggest("$x")
 
-	m.Match("float32($x)").Where(m["x"].Type.Is("float32")).Report("unnecessary conversion").Suggest("$x")
-	m.Match("float64($x)").Where(m["x"].Type.Is("float64")).Report("unnecessary conversion").Suggest("$x")
+	m.Match("float32($x)").Where(m["x"].Type.Is("float32") && !m["x"].Const).Report("unnecessary conversion").Suggest("$x")
+	m.Match("float64($x)").Where(m["x"].Type.Is("float64") && !m["x"].Const).Report("unnecessary conversion").Suggest("$x")
 
 	// m.Match("byte($x)").Where(m["x"].Type.Is("byte")).Report("unnecessary conversion").Suggest("$x")
 	// m.Match("rune($x)").Where(m["x"].Type.Is("rune")).Report("unnecessary conversion").Suggest("$x")
-	m.Match("bool($x)").Where(m["x"].Type.Is("bool")).Report("unnecessary conversion").Suggest("$x")
+	m.Match("bool($x)").Where(m["x"].Type.Is("bool") && !m["x"].Const).Report("unnecessary conversion").Suggest("$x")
 
-	m.Match("int8($x)").Where(m["x"].Type.Is("int8")).Report("unnecessary conversion").Suggest("$x")
-	m.Match("int16($x)").Where(m["x"].Type.Is("int16")).Report("unnecessary conversion").Suggest("$x")
-	m.Match("int32($x)").Where(m["x"].Type.Is("int32")).Report("unnecessary conversion").Suggest("$x")
-	m.Match("int64($x)").Where(m["x"].Type.Is("int64")).Report("unnecessary conversion").Suggest("$x")
+	m.Match("int8($x)").Where(m["x"].Type.Is("int8") && !m["x"].Const).Report("unnecessary conversion").Suggest("$x")
+	m.Match("int16($x)").Where(m["x"].Type.Is("int16") && !m["x"].Const).Report("unnecessary conversion").Suggest("$x")
+	m.Match("int32($x)").Where(m["x"].Type.Is("int32") && !m["x"].Const).Report("unnecessary conversion").Suggest("$x")
+	m.Match("int64($x)").Where(m["x"].Type.Is("int64") && !m["x"].Const).Report("unnecessary conversion").Suggest("$x")
 
-	m.Match("uint8($x)").Where(m["x"].Type.Is("uint8")).Report("unnecessary conversion").Suggest("$x")
-	m.Match("uint16($x)").Where(m["x"].Type.Is("uint16")).Report("unnecessary conversion").Suggest("$x")
-	m.Match("uint32($x)").Where(m["x"].Type.Is("uint32")).Report("unnecessary conversion").Suggest("$x")
-	m.Match("uint64($x)").Where(m["x"].Type.Is("uint64")).Report("unnecessary conversion").Suggest("$x")
+	m.Match("uint8($x)").Where(m["x"].Type.Is("uint8") && !m["x"].Const).Report("unnecessary conversion").Suggest("$x")
+	m.Match("uint16($x)").Where(m["x"].Type.Is("uint16") && !m["x"].Const).Report("unnecessary conversion").Suggest("$x")
+	m.Match("uint32($x)").Where(m["x"].Type.Is("uint32") && !m["x"].Const).Report("unnecessary conversion").Suggest("$x")
+	m.Match("uint64($x)").Where(m["x"].Type.Is("uint64") && !m["x"].Const).Report("unnecessary conversion").Suggest("$x")
 }
